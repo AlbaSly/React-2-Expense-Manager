37af80a73a515204f52ff6460bb435bf0bc212f7
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-23 18:35:24 -0500

    Change H1 Title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header = (props: HeaderComponentProps): JSX.Element => {
 
     return (
         <header>
-            <h1>RaxExpense Manager</h1>
+            <h1>Expense Manager</h1>
 
             {
                 !validatedBudget
